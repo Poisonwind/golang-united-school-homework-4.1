f0d7c22e346eb1508ebe7c30b03e6d53df44e323
Poisonwind <pogorelchukpa@gmail.com>
2022-05-14 10:20:02 +0300

    solution

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,16 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+
+	runeArray := []rune(input)
+
+	for i, j := 0, len(runeArray)-1; i < j; i, j = i+1, j-1 {
+
+		runeArray[i], runeArray[j] = runeArray[j], runeArray[i]
+
+	}
+
+	output = string(runeArray)
+
 	return output
 }
